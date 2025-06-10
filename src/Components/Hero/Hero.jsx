4e8f0128393e60e27bd44bf781c9777cfd6b82bf
Ethan Dhavaran <dhavaranethan@gmail.com>
2025-06-10 23:05:23 +0200

    addition of cv

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Hero.css';
 import profile_img from '../../assets/profile_img.jpg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import cv from '../../assets/CV.pdf';
 
 const Hero = () => {
     return (
@@ -10,9 +11,9 @@ const Hero = () => {
             <h1><span>I am Ethan Jordan Dhavaran, </span>currently in my third year of BSc Computer Science</h1>
             <section className="hero-action">
                 <section className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></section>
-                <section className="hero-resume">My resume</section>
-            </section>
+                <a href={cv} target="_blank" rel="noopener noreferrer" download className="hero-resume">My resume</a>
         </section>
+        </section >
     );
 };
 
